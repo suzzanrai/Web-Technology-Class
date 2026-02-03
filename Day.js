b3b51b2f65358bcfaa26d23192c71ea39ebe4020
suzzanrai <suzzanrai@gmail.com>
2026-02-03 08:09:26 +0545

    learning email validation and creating function

--- Day.js
+++ Day.js
@@ -64,7 +64,50 @@ console.log("Average is:",sum/Var.length);
 
 let text = "love";
 let rule = /l/;
-let text2 = "12345";
-let rule2 =  /[0-9]/;
-console.log(rule2.test(text2));
+//let rule3 = /[a-z]/; // for small letters
+let rule3 = /[A-Z]/; // for capital letters
+// let rule = /[a-zA-Z]/; // for both small and capital letters
+// let rule = /[0-9]/; // for digits
 console.log(rule.test(text)); 
+console.log(rule3.test(text)); 
+
+let text2 = "12345";
+let rule2 = /\d/ ;
+//  Or, /[0-9]/;
+console.log(rule2.test(text2));
+
+let text4 = "sujan";
+let rule4 = "/s.n/"; // . means any one character but should start with s and end with n 
+console.log(rule.test(text4));
+
+
+
+      // Regular Expression Symbols NOTES
+// . = any one character
+// * = 0 or more times
+// + = 1 or more times
+// ? = 0 or 1 time
+// ^ = starts with
+// $ = ends with
+// [ ] = anyone or these (range)
+// {n} = exact n of times
+
+// EXAMPLES
+ let text5 = "sujan@123";
+ let rule5 = /^(?=.*[A-Z]) (?=.*[a-z]) (?=.*\d) (?=.*[@$!%*?&]) [A-Za-z\d@$!%*?&] {8,}$/;
+ console.log(rule5.test(text5));
+
+ //DAY 5
+
+ //email validation
+
+let email ="suj.an123@gmail.com";
+              // first line  //@ //forGmail //for. //com
+let emailrule = /^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+$/
+console.log("email validate");
+console.log (emailrule.test(email));
+
+console.log(".np validation");
+let np="ankit.55624mmamc.tu.edu.np";
+let nprule= /^[a-zA-Z0-9]+\.[a-zA-Z]+\.[a-zA-Z] +\.[a-zA-Z]/;
+console.log(nprule.test(np));
